832ca1ba63573accfa65af211d0edc0d6d6660b5
Clément Ronzon <clem.rz@gmail.com>
2020-07-18 15:51:36 -0500

    Solves race-condition related issues

--- src/components/query/Query.js
+++ src/components/query/Query.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import moment from 'moment';
 
 type Props = {|
@@ -27,34 +27,47 @@ const Query = React.memo((props: Props) => {
 
   useEffect(() => {
     if (props.startDate && props.endDate) {
+
+      const abortController = new AbortController(); // For in-flight abortion, solves race-conditions related issues
+
       const data = {
         startDate: moment.utc(props.startDate).unix(),
         endDate: moment.utc(props.endDate).unix(),
         symbol: SYMBOL
       };
+
       setIsFetching(true);
+
       fetch(props.url, {
         method: props.method || 'GET',
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
+        signal: abortController.signal
       })
         .then(res => res.json())
         .then(
           (response) => {
+            if (abortController.signal.aborted) {
+              return;
+            }
             setIsFetching(false);
             setResponse(response);
           },
-          // Note: it's important to handle errors here
-          // instead of a catch() block so that we don't swallow
-          // exceptions from actual bugs in components.
           (error) => {
+            if (abortController.signal.aborted) {
+              return;
+            }
             setIsFetching(false);
             setError(error);
           }
         );
+
+      return () => {
+        abortController.abort();
+      };
     }
   }, [props]);
   return props.children({error, fetching, data: response});
